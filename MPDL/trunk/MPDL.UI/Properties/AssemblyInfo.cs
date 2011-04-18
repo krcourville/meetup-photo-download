957d1445315780be0fab32f66cefcd10d51c5c94
krcourville@gmail.com <krcourville@gmail.com@bf8ded14-ab8f-d853-24d7-d390cdfcff38>
2011-04-18 14:12:48 +0000

    Updated assembly versions to 2.0.0.2

--- MPDL/trunk/MPDL.UI/Properties/AssemblyInfo.cs
+++ MPDL/trunk/MPDL.UI/Properties/AssemblyInfo.cs
@@ -47,5 +47,5 @@ using System.Windows;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("2.0.0.1")]
-[assembly: AssemblyFileVersion("2.0.0.1")]
+[assembly: AssemblyVersion("2.0.0.2")]
+[assembly: AssemblyFileVersion("2.0.0.2")]
